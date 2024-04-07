afa666828844e303b74dd17d801d36cdec4ceaae
e277 <ezramuir12@gmail.com>
2024-04-07 16:28:43 -0500

    Created these files for implementation

---
+++ src/Notification.java
@@ -0,0 +1,5 @@
+public class Notification {
+//    write a method that uses the JOptionPane class to display a message dialog with the message "Invalid ID"
+//    after sending an email to the security guard with the message "Invalid ID"
+
+}
